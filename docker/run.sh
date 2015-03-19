8caa2c104d1656c5e42801c4cf3e5e3df2f4d5fd
majidaldo <majidaldosari-github@yahoo.com>
2015-03-19 19:37:43 -0400

    notes about how to run dockre in docker

--- docker/run.sh
+++ docker/run.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
 
+#this is for interactive/dev use?
+
 docker run \
        -t -i \
        --privileged \ 
        --volume /tsad-proj:/tsad-proj \
        mydind # my docker in docker
 
+#GPU access pts could go here
