df094a821c93c0b1b7777723303aa9aa9ceb7913
majidaldo <majidaldosari-github@yahoo.com>
2015-03-25 15:10:47 -0400

    option on running which docker in docker

--- docker/run.sh
+++ docker/run.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+if [ $# -ge 1 ]
+then
+    MYDIND=$1
+else
+    MYDIND=mydind
+fi
+
 
 #this is for interactive/dev use?
 ## To detach the tty without exiting the shell,
@@ -9,7 +16,7 @@ docker run \
        --privileged \
        --volume /tsad-proj:/tsad-proj \
        --env-file /tsad-proj/tsad-sys/env \
-       mydind # my docker in docker
+       $MYDIND # my docker in docker
 
 #volume main purpose is for pushing code in from host for dev
 #GPU access pt could go in the run cmd
