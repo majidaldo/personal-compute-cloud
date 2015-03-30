a7d1989b074967ee41a5ecb7c02ccd12c48a6b2e
majidaldo <majidaldosari-github@yahoo.com>
2015-03-30 18:22:08 -0400

    clarification on vagrant setup automation

--- vagrant/setup.sh
+++ vagrant/setup.sh
@@ -13,10 +13,11 @@ if [ $(cat firsttime.state) = "1" ]
 then
     #vagrant plugin installs could be here
     source ./get_envs
-    #to avoid recursion
-    mv setup.sh dontrunsetup.sh
+    #to avoid recursion...
+    mv ./setup.sh ./dontrunsetup.sh
+    #...when running this script from a vagrantfile
     ./vagrant box add --force --name $DOCKER_BOX $DOCKER_URL
-    mv dontrunsetup.sh setup.sh
+    mv ./dontrunsetup.sh ./setup.sh
     if [ $? -ne 0 ] #err on above line
     then
 	exit $?
