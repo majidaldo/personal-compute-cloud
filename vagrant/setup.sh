6f2654a1a762deb59ae72ed4d497e0f920852098
majidaldo <majidaldosari-github@yahoo.com>
2015-04-09 16:44:50 -0400

    coreos init

--- vagrant/setup.sh
+++ vagrant/setup.sh
@@ -12,13 +12,12 @@ fi
 if [ $(cat firsttime.state) = "1" ]
 then
     #vagrant plugin installs could be here
-    source ./get_envs
+    vagrant plugin install vagrant-winnfsd
+    source ./get_envs && \
     #to avoid recursion...
-    mv ./setup.sh ./dontrunsetup.sh
+    mv ./setup.sh ./renameto_setup.sh && \
     #...when running this script from a vagrantfile
-    ./vagrant box add --force --name $DOCKER_BOX $DOCKER_URL
-    ./vagrant box add --force --name $REGISTRY_BOX $REGISTRY_URL
-    mv ./dontrunsetup.sh ./setup.sh
+    mv ./renameto_setup.sh ./setup.sh
     if [ $? -ne 0 ] #err on above line
     then
 	exit $?
