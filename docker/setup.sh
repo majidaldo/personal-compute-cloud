0b29eacb0177c2a9be48b6f4daf69477df1dc5e3
majidaldo <majidaldosari-github@yahoo.com>
2015-05-08 19:26:07 -0400

    not using dind or data imgs

--- docker/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-#1. build docker in docker
-#2. enter the docker 
-#3. build dockers (can it be done from this script?)
-
-#todo: possible to output the container id to give it to another cmd?
-docker build -t mydind dind/
-
