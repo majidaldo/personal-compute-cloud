4f2b1b2b3f7373eba15ea364e6a11acec5e7186f
majidaldo <majidaldosari-github@yahoo.com>
2015-05-16 20:10:23 -0400

    looks like this fixes building b/w instances BUT still always a problem with d2n2

--- docker/build.sh
+++ docker/build.sh
@@ -4,14 +4,21 @@
 #builds and pushes into pvt registry.
 #cleans up too
 
+#exit
+
+
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd $SCRIPTPATH
 
-for adfdir in $(ls -d1 */)
+#docker file dirs should have digits followed by -
+#like 123-dockerfiledir
+#order in the files is needed to take care of deps
+for adfdir in $(ls -d1 */ | grep -Ei '[0-9]-')
 do
-    #use dir as a name. remove slashes
-    name=$(echo $adfdir | sed 's/\///')
+    #use dir as a name. numbers
+    #                  remove / from dir/   remove "number-"
+    name=$(echo $adfdir | sed 's/\///' | sed 's/[0-9]*-//')
     echo building $name
     #start with it if it's already there
     docker pull          $REGISTRY_HOST/$name
@@ -19,12 +26,14 @@ do
     echo loading into registry \
 	                 $REGISTRY_HOST/$name
     #assuming repos in 'root' ( library/ )
-    #overwrite if it's already there
-    docker tag -f $name  $REGISTRY_HOST/$name
+    docker rmi           $REGISTRY_HOST/$name
+    docker tag    $name  $REGISTRY_HOST/$name
     docker push          $REGISTRY_HOST/$name
     docker rmi           $REGISTRY_HOST/$name
-    docker rmi                          $name
+    #docker rmi                          $name
 done
 
+
+#since i'm remounting /var/lib/docker no need to repull stuff from registry
 #extra. only the 'base' images should be left
-docker rmi $(docker images -q)
+#docker rmi $(docker images -q) | :
