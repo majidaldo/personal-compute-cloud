01e4e036b9fdd9900aca87a56cc5ae02bc809f24
Majid d <majidaldosari-github@yahoo.com>
2015-06-24 18:21:37 -0400

    gets template files for ansible project

---
+++ get-ansible-template.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -e
+
+#setup
+rm -fr ansible-temp
+mkdir ansible || true
+git clone --recursive git@github.com:majidaldo/ansible-vagrant.git ansible-temp
+
+#get files
+cd ansible-temp
+git checkout template 
+git checkout master $(cat files.txt) 
+
+#copy over
+#force overwrite
+#cp -rf $(cat files.txt) ../ansible
+#only files that don't exist. -u doesn't do the job
+yes n | cp -r $(cat files.txt) ../ansible
+
+#clean up
+cd ..
+rm -fr ansible-temp
