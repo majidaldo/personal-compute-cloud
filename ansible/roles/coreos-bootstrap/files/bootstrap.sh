78425e95fd2e097fc01b4215b25a156726b594a6
Majid d <majidaldosari-github@yahoo.com>
2015-07-09 18:25:57 -0400

    reorganized for ansible injecting config into vagrantfile

---
+++ ansible/roles/coreos-bootstrap/files/bootstrap.sh
@@ -0,0 +1,30 @@
+#/bin/bash
+
+set -e
+
+cd
+
+if [[ -e $HOME/.bootstrapped ]]; then
+  exit 0
+fi
+
+PYPY_VERSION=2.4.0
+
+wget -O - https://bitbucket.org/pypy/pypy/downloads/pypy-$PYPY_VERSION-linux64.tar.bz2 |tar -xjf -
+mv -n pypy-$PYPY_VERSION-linux64 pypy
+
+## library fixup
+mkdir -p pypy/lib
+ln -snf /lib64/libncurses.so.5.9 $HOME/pypy/lib/libtinfo.so.5
+
+mkdir -p $HOME/bin
+
+cat > $HOME/bin/python <<EOF
+#!/bin/bash
+LD_LIBRARY_PATH=$HOME/pypy/lib:$LD_LIBRARY_PATH exec $HOME/pypy/bin/pypy "\$@"
+EOF
+
+chmod +x $HOME/bin/python
+$HOME/bin/python --version
+
+touch $HOME/.bootstrapped
