bcc378221be7aab2d267dbff979b4b7c8c69a2ee
Majid d <majidaldosari-github@yahoo.com>
2015-07-22 11:27:45 -0400

    fix etcd config

--- config/coreos/cconfig.sh
+++ config/coreos/cconfig.sh
@@ -2,10 +2,15 @@
 
 scriptDIR=$(dirname $0)
 cd $scriptDIR
+
 if [ "$1" = "init" ]
 then
-    ./constructor.sh master.yaml init.skeleton.yaml id.init.env init.env
-#elif [     ]
+    echo -n "DISCOVERY_URL=" > discovery.url
+    curl -w "\n" 'https://discovery.etcd.io/new?size=1' >> discovery.url
+    ./constructor.sh master.yaml init.skeleton.yaml global.env discovery.url id.init.env init.env
+elif [ "$1" = "compute_local"    ]
+then
+    ./constructor.sh master.yaml compute.skeleton.yaml global.env discovery.url compute.env
 else echo "choose configuration"
 exit 1
 fi
