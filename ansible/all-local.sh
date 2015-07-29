6259c9cedada45353b070e81fa8156c6be8b387c
Majid d <majidaldosari-github@yahoo.com>
2015-07-29 15:55:36 -0400

    lil util that... DESTROYYYS!

--- ansible/all-local.sh
+++ ansible/all-local.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+destroy=${1-dontdestroy}
+
+if [ $destroy = "destroy" ]; then
+   ./destroy-vagrant.sh
+fi
+
 ansible-playbook -vvvv init.yml &
 ansible-playbook -vvvv compute-local.yml &
 wait
