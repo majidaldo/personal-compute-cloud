de17092634ee0c6b695a360d425caa034a5a46fb
Majid d <majidaldosari-github@yahoo.com>
2015-07-30 18:38:44 -0400

    make registry

--- setup/makeregistry.sh
+++ setup/makeregistry.sh
@@ -3,12 +3,12 @@
 
 
 
-if [ ! -f /tsad-proj/tsad-bigfiles/images/docker/bootstrap/registry.tar ];
+if [ ! -f /project/files/images/docker/bootstrap/registry.tar ];
 then
-    mkdir -p /tsad-proj/tsad-bigfiles/images/docker/bootstrap
-    cd /tsad-proj/tsad-sys/docker
+    mkdir -p /project/files/images/docker/bootstrap
+    cd /project/docker
     docker build -t docker-registry docker-registry 
-    docker save -o /tsad-proj/tsad-bigfiles/images/docker/bootstrap/registry.tar \
+    docker save -o /project/files/images/docker/bootstrap/registry.tar \
         docker-registry
 fi
  
