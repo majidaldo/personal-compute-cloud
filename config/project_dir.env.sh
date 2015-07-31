301b960c1cf6229634756dd2619e7863237fbce7
Majid d <majidaldosari-github@yahoo.com>
2015-07-31 01:22:19 -0400

    project dir root now a default and is a more proper value

--- config/project_dir.env.sh
+++ config/project_dir.env.sh
@@ -2,17 +2,24 @@
 
 #b/c the dir structure exits in the local machine
 #..and the 'remote' (rooted) machine with different paths
+# 
 
 thisdir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd $thisdir
+
 PROJECT_DIR_REL=$thisdir/../
-PROJECT_DIR_ROOT=${2-/project}
+source ./project.env
+PROJECT_DIR_ROOT=${2-${PROJECT_DIR_ROOT}}
+
+
+
 
 pthtype=${1-notgiven}
 
-if [ $pthtype == "remote" ]
+if [ $pthtype = "remote" ]
 then
-    PROJECT_DIR=$PROJECT_DIR_ROOT
-elif [ $pthtype == "local" ]
+    PROJECT_DIR=${PROJECT_DIR_ROOT-$3}
+elif [ $pthtype = "local" ]
 then
     PROJECT_DIR=$PROJECT_DIR_REL
 else
@@ -20,5 +27,6 @@ else
  second arg can be the remote project root path"
     exit 1
 fi
+
 
 echo $PROJECT_DIR
