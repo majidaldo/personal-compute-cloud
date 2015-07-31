263168c7fac4b4b70648dff71777843a22fd89fc
Majid d <majidaldosari-github@yahoo.com>
2015-07-31 17:28:20 -0400

    creaion of /var/lib/docker in setup process

--- setup/projectfiles.sh
+++ setup/projectfiles.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
+source ../common.src
+cd $SCRIPT_DIR
 
-vagrant up
-vagrant destroy -f
+
+./vagrant up  --no-provision
+
+
+./vagrant provision --provision-with \
+	  "docker registry img creation"
+
+if [ `$ENVCMD CREATE_VARLIBDOCKER_INSETUP` = true ]
+then
+    ./vagrant provision --provision-with \
+	      "btrfs f/s creation for /var/lib/docker"
+fi
+
+./vagrant destroy -f
+
