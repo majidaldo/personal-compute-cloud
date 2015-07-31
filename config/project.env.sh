11fd04b8425e0d0b559e45e12b50b8444719c0ee
Majid d <majidaldosari-github@yahoo.com>
2015-07-31 02:01:57 -0400

    spits out an env var with local or remote pathing for the path vars

--- config/project.env.sh
+++ config/project.env.sh
@@ -1,28 +1,53 @@
 #!/bin/bash
+set -e
 
-# input the env var for the first arg. then optionally
-# specify remote|local pathing for the second arg
+if [ "$1" == "--help" ]
+then
+    hm="input the env var for the first arg. then optionally \
+     specify remote|local pathing for the second arg \
+     if you specify the second arg, then the 3rd would be \
+     optionally the root directory of the project"
+    echo $hm
+    exit
+fi
+
+
+if [ "$2" != "remote" ]
+then
+    if [ "$3" != "" ]
+    then
+	echo "must have 'remote' as the arg before $3."
+	exit 1
+    fi
+fi
+
+rorl=${2-remote}
+if [ "$2" != "" ]
+then
+    rorl=remote
+else
+    rorl=local
+fi
 
 thisdir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $thisdir
 
-
 ld=$(echo ${thisdir}/project_dir.env.sh local)
 echo PROJECT_DIR=$ld >  ${thisdir}/local-project_dir.env.tmp
-
 rd=$(echo ${thisdir}/project_dir.env.sh remote)
 echo PROJECT_DIR=$rd > ${thisdir}/remote-project_dir.env.tmp
 
 
-rorl=${2-remote} #local is the other option
-
-
 #cool syntax:  cmdthattakesfilearg <( cmdthatprints )
+#..but  the following only works in bash
 source <(./compose_env.sh \
-	   <(echo PROJECT_DIR=$(./project_dir.env.sh $rorl)) \
+	   <(echo PROJECT_DIR=$(./project_dir.env.sh $rorl $3 )) \
 	   project.env \
 	   coreos/global.env \
    )
 
 
 eval echo \$$1
+
+
+
