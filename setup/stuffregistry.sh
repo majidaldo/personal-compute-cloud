96b79cd886f8921bfdcc31ccbe7ba0bbe405bb14
Majid d <majidaldosari-github@yahoo.com>
2015-07-31 22:46:51 -0400

    stuff registry from setup

---
+++ setup/stuffregistry.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+source ../common.src
+
+
+regdatadir=`$ENVCMD REGISTRYDATA_DIR` 
+regimgdir=`$ENVCMD REGISTRYIMG_DIR`
+
+
+docker load --input ${regimgdir}/registry.tar
+mkdir -p $regdatadir
+docker run \
+        --name registry \
+        -d \
+        -v ${regdatadir}:/registry \
+        -e STORAGE_PATH=/registry \
+        -e SETTINGS_FLAVOR=dev \
+        -e GUNICORN_OPTS=[--preload] \
+        -e MIRROR_SOURCE=https://registry-1.docker.io \
+        -e MIRROR_SOURCE_INDEX=https://index.docker.io \
+        -p 5000:5000 \
+        docker-registry
+
+
+export REGISTRY_HOST=localhost:5000
+cd `$ENVCMD PROJECT_DIR`/docker
+./build.sh
+
+
+docker stop registry
+docker rm registry
+
