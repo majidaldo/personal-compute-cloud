19213108c2e6a7d983bad948d47693ece49c27dd
Majid d <majidaldosari-github@yahoo.com>
2015-08-03 10:28:38 -0400

    compute works

--- config/coreos/cconfig.sh
+++ config/coreos/cconfig.sh
@@ -6,21 +6,27 @@ source ../../common.src #.. then find my common stuff
 #variable 'imports'
 pd="echo PROJECT_DIR=`$ENVCMD PROJECT_DIR remote`"
 pf="echo PROJECT_FILES=`$ENVCMD PROJECT_FILES remote`"
-nfs="echo NFS_SHARE=`$ENVCMD NFS_SHARE`"
+ns="echo NFS_SHARE=`$ENVCMD NFS_SHARE`"
 
 if [ "$1" = "init" ]
 then
     ./constructor.sh master.yaml init.skeleton.yaml \
 		     <($pd) \
 		     <($pf) \
-      		     <($nfs) \
+      		     <($ns) \
 		     global.env \
 		     discovery.url.tmp \
 		     id.init.env \
 		     init.env
 elif [ "$1" = "compute"    ]
 then
-    ./constructor.sh master.yaml compute.skeleton.yaml <($pd) global.env discovery.url.tmp compute.env
+    ./constructor.sh master.yaml compute.skeleton.yaml \
+		     <($pd) \
+		     <($fp) \
+		     <($ns) \
+		     global.env \
+		     discovery.url.tmp \
+		     compute.env
 else echo "choose configuration"
 exit 1
 fi
