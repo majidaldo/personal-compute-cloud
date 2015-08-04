af7383c7a09882114f193d90b14bb1e0c5a1b6fa
Majid d <majidaldosari-github@yahoo.com>
2015-08-04 16:00:26 -0400

    find todos in this repo. cool lil util

---
+++ find_todos.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+source ./common.src
+
+pf=$(basename `$ENVCMD PROJECT_FILES`)
+files=$(find ./ -type f \
+	     -not -path "./files/*" \
+	     -not -path "*/.git/*" \
+	     -not -path "*.tmp" \
+	     -not -path "./find_todos.sh*" \
+     )
+
+
+for af in $files
+do
+
+    matches=`cat $af | grep -n -B 5 -A 5 'todo' --color=always`
+    if [ "$matches" != "" ]
+    then
+	echo $af------
+	echo
+	printf "$matches"
+	echo
+	echo -------------------------------
+	echo
+	echo
+    fi
+
+done
