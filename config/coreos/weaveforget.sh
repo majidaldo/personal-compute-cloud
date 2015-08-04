4519e83537c33f3e070aac4e9f49249483fe1790
Majid d <majidaldosari-github@yahoo.com>
2015-08-04 08:34:28 -0400

    run this to kick out dropped clients

---
+++ config/coreos/weaveforget.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+
+#weave port
+wp='6783'
+re='(?<=\[)(.*?)(?=:'${wp}'])'
+#connection timeout
+to=5
+#bad and not scalable
+#nccmd="docker run --rm busybox nc"
+#does not have the port scanner!
+nccmd="ncat" 
+
+while true
+do
+    #status is only app up to Reconn section
+    peers=`weave status | sed -e 's/Peers:\(.*\)Reconnects:/\1/' | pcregrep -o -e $re`
+    #echo $peers; exit
+    for apr in $peers
+    do
+	#check connectivity to peer
+	ncr=`echo yousuck | $nccmd -w $to $apr $wp 2>/dev/null | grep -o -e weave`
+        #interestingly the netcat probe returns 'weave'
+	#if [ $? != 0 ] #wrks with the docker cmd
+	if [ "${ncr}" != "weave" ] #since scanner opt not avail in ncat
+	then
+	    weave forget $apr
+	    echo $apr down. weave forgetting.
+	else
+	    #echo $apr ok
+	    :
+	fi
+    done
+    sleep 10
+done
+ 
+
