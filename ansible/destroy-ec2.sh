7eaa7a082359b26f27747e463c75a7e3cab4427d
Majid d <majidaldosari-github@yahoo.com>
2015-08-15 09:10:39 -0400

    only setup ec2 if number of machines requested is bigger than 0

--- ansible/destroy-ec2.sh
+++ ansible/destroy-ec2.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 
-
-# todo: remove ec2inst dependence on ec2setup?
-# so that when i destroy i dont have to exec ec2setup
-
 ansible-playbook -vvvv ec2.yml -e count=0 -e type=compute
 ansible-playbook -vvvv ec2.yml -e count=0 -e type=gpu
