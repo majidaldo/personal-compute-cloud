94b832591d94da4ae6a1f89bc5c4592ef123a2ea
Majid d <majidaldosari-github@yahoo.com>
2015-10-06 21:53:28 -0400

    init weave 1.1.1

--- config/coreos/weaveforget.sh
+++ config/coreos/weaveforget.sh
@@ -13,13 +13,20 @@ nccmd="/usr/bin/ncat"
 
 while true
 do
-    #status is only app up to Reconn section
-    peers=`/opt/bin/weave status | /usr/bin/sed -e 's/Peers:\(.*\)Reconnects:/\1/' | /usr/bin/pcregrep -o -e $re`
-    #echo $peers; exit
-    for apr in $peers
+    # assuming index 0 is self
+    mypeers=`/opt/bin/weave report | \
+             /usr/bin/jq '.Router.Peers[0].Connections'`
+    npeers=`echo $mypeers | /usr/bin/jq length`
+    for ((api=0;api<$npeers;api++)) # a peer index
     do
+
+	# a peer
+	apr=`/usr/bin/echo $mypeers | \
+             /usr/bin/jq -r ".[${api}].Address"  `
+	aprip=`echo $apr | sed 's/:[0-9]*//'` #just the ip address
 	#check connectivity to peer
-	ncr=`echo yousuck | $nccmd -w $to $apr $wp 2>/dev/null | grep -o -e weave`
+	ncr=`echo yousuck | $nccmd -w $to $aprip $wp 2>/dev/null | grep -o -e weave`
+	
         #interestingly the netcat probe returns 'weave'
 	#if [ $? != 0 ] #wrks with the docker cmd
 	if [ "${ncr}" != "weave" ] #since scanner opt not avail in ncat
@@ -27,7 +34,7 @@ do
 	    /opt/bin/weave forget $apr
 	    echo $apr down. weave forgetting.
 	else
-	    #echo $apr ok
+	    echo $apr ok
 	    :
 	fi
     done
