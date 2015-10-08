7dfc8a955331719d31a110d89f44133db386ccfd
Majid d <majidaldosari-github@yahoo.com>
2015-10-07 21:19:36 -0400

    update registry images in setup

--- setup/registry_img.sh
+++ setup/registry_img.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 set -e
 
+cd $(dirname $0)
 source ../common.src
+
 drid=`$ENVCMD REGISTRYIMG_DIR` #docker registry img dir
 #local or remote wont matter. resolves to same pth
 
@@ -9,7 +11,12 @@ if [ ! -f ${drid}/registry.tar ];
 then
     mkdir -p $drid
     cd `$ENVCMD PROJECT_DIR`/docker
-    docker pull                         distribution/registry
-    docker save -o ${drid}/registry.tar distribution/registry
+    docker pull                              distribution/registry
+    docker save -o      ${drid}/registry.tar distribution/registry
+else #update it
+    docker load --input ${drid}/registry.tar | : #ignr if crptd
+    docker pull                              distribution/registry
+    rm                  ${drid}/registry.tar
+    docker save -o      ${drid}/registry.tar distribution/registry
 fi
 
